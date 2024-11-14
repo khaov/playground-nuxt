767dc06af7bdf468c561f2fe6fe5b3e069fae179
Artyom Khaov <a.khaov@gmail.com>
2024-11-14 15:08:33 +0700

    build: upgrading to Nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: "2024-11-13"
 })
