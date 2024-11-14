5039adc1b04ffe534cbbf73603106a0c198cc7c3
Artyom Khaov <a.khaov@gmail.com>
2024-11-14 17:33:38 +0700

    feat: added SharedCard basic structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+  const items = [
+    { title: "Быстрая выдача", text: "Вы получите деньги через 10 минут", image: "/images/item-1.png" },
+    { title: "Надежность", text: "Все ваши данные защищены", image: "/images/item-2.png" },
+  ];
+</script>
+
 <template>
   <h1>Frontend Projects</h1>
   <p>
     Project ideas to take you from beginner to advanced in Frontend
   </p>
+
+  <div>
+    <SharedCard
+      v-for="(item, index) in items" :key="index"
+      class=""
+    >
+    <span>
+      {{ item.title }}
+    </span>
+  </SharedCard>
+  </div>
+
 </template>
